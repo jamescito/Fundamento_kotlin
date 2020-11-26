ccc361371ec324d0242f2ecf31df221fd7e04bd6
jamescito <67704896+jamescito@users.noreply.github.com>
2020-11-25 19:57:21 -0600

    creamos el fichero tipos e hicimos los ejercicio de este

---
+++ tipos.kt
@@ -0,0 +1,20 @@
+fun main()
+{
+    //numeros
+val int = 123
+val long = 123456L
+val double = 12.34
+val float = 12.34F
+val hexadecimal = 0xAB
+val binary = 0b01010101
+
+println("$int $long $double $float $hexadecimal $binary")
+//booleanos
+val x = 1
+val y = 2
+val z = 2
+val esTrue = x < y && x < z
+val otroTrue = x == y || y == z
+println("$x $y $z $esTrue $otroTrue")
+}
+
