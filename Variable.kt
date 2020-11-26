f9d946ea653c1fad9948991e0f8f147e043d71b6
jamescito <67704896+jamescito@users.noreply.github.com>
2020-11-25 19:36:55 -0600

    creamos el fichero variable e hicimos el primer ejercicio

---
+++ Variable.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    var nombre ="James"
+
+    nombre ="Alberth"
+
+ var apellido = "Reyes"
+ 
+ apellido = "Calderon"
+
+ var edad = 20
+ println("Mis nombre son $nombre $apellido y mi edad es $edad")
+
+}
