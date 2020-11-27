e35de874345e0af9ea5be266a60dccbfbcb4b2fe
jamescito <67704896+jamescito@users.noreply.github.com>
2020-11-27 10:01:10 -0600

    modificamos los if

--- practica-2/if.kt
+++ practica-2/if.kt
@@ -1,5 +1,15 @@
+import java.util.date
+
+
 fun main ()
 {
+    val fecha = Date()
+val hoy = if (fecha.year == 2019) true else false
+
+    
+    
+    
+    
     print("Ingrese una edad")
     val edad = readLine()?.toInt() as Int
 if(edad >= 20)
