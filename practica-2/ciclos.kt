2cd82704f119845051fc4ee823eb8c4e97071ac4
jamescito <67704896+jamescito@users.noreply.github.com>
2020-11-27 10:00:49 -0600

    modificamos los ciclos

--- practica-2/ciclos.kt
+++ practica-2/ciclos.kt
@@ -1,18 +1,38 @@
 fun main()
 {
     //while
-/*while (1>0) {
+    var i:Int=0
+while (i<=5) {
     println("Esto se imprimirá por un largo tiempo!")
-    i-=1
-   }*/
+    i++
+   }
 
  val array = arrayOf("a", "b", "c")
 for (i in array.indices) {
  println(array[i])
 }
+
+for (i in 2 downTo 0 step 1) {
+    println(array[i])
+   }
+      
+   for (j in 0..2 ) {
+    println(array[j])
+   }
+       
+   
+   
+
 for (i in 6 downTo 0 step 2) {
  println(i)
 }
    
+//do while
+var a:Int=0
+do{
+    println("soy do while")
+    a++
+}while (a <=3)
+
 }
 
