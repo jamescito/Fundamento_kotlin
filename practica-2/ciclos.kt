72e5cb1b5572e9cdedef1d9eddac572402ed8c87
jamescito <67704896+jamescito@users.noreply.github.com>
2020-11-26 22:11:21 -0600

    Agregamos los ciclos while y for

---
+++ practica-2/ciclos.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+    //while
+/*while (1>0) {
+    println("Esto se imprimirá por un largo tiempo!")
+    i-=1
+   }*/
+
+ val array = arrayOf("a", "b", "c")
+for (i in array.indices) {
+ println(array[i])
+}
+for (i in 6 downTo 0 step 2) {
+ println(i)
+}
+   
+}
+
---
+++ practica-2/when.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    val calificacion=50
+when(calificacion)
+{
+ in 10..20 -> println("hay que motivarse por completo")
+ in 30..50 -> println("demuestra un poco mas de trabajo")
+ in 50..59 -> println("por poco")
+ 60 -> println("estuvo peligroso esta operacion")
+ in 61..80 -> println("sigamos adelante")
+ in 80..100 -> println("Excelente resultado")
+ !in 1..100 -> println("no conozco ese resultado")
+ else -> println("sigo sin conocer esto")
+}
+
+}
